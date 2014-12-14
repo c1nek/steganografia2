74207309271b2a8bda929f6c1718c3fd439abf9c
c1nek <gumball300@gmail.com>
2014-12-14 21:30:39 +0100

    guwno podejscie 2
    szyfrowanie w koncu działa XD

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace steganografia2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oProgramieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zakończToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 37);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(628, 86);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button1.Location = new System.Drawing.Point(13, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Wczytaj obraz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button2.Location = new System.Drawing.Point(13, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Ukryj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button3.Location = new System.Drawing.Point(13, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Zapisz obraz";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(118, 129);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(628, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(752, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 222);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1003, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oProgramieToolStripMenuItem,
+            this.zakończToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // oProgramieToolStripMenuItem
+            // 
+            this.oProgramieToolStripMenuItem.Name = "oProgramieToolStripMenuItem";
+            this.oProgramieToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.oProgramieToolStripMenuItem.Text = "O programie";
+            // 
+            // zakończToolStripMenuItem
+            // 
+            this.zakończToolStripMenuItem.Name = "zakończToolStripMenuItem";
+            this.zakończToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.zakończToolStripMenuItem.Text = "Zakończ";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 35);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 28);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Wczytaj tekst";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 371);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oProgramieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zakończToolStripMenuItem;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+
+namespace steganografia2
+{
+    public partial class Form1 : Form
+    {
+        String bmpPath;
+        Bitmap imageText;
+        String xbmpPatch;
+        Bitmap ximage;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private String string2bin(String wej)
+        {
+            byte[] btText;
+            btText = System.Text.Encoding.UTF8.GetBytes(wej); 
+            Array.Reverse(btText); 
+            BitArray bit = new BitArray(btText); 
+            StringBuilder sb = new StringBuilder(); 
+        
+            for (int i = bit.Length - 1; i >= 0; i--) 
+                { 
+                if (bit[i] == true) 
+                    { 
+                        sb.Append(1); 
+                    } 
+                else 
+                    { 
+                        sb.Append(0); 
+                    } 
+                }
+
+        return sb.ToString();
+
+        }
+        private Bitmap hide(Bitmap image, String text)
+        {  
+            String textBits = string2bin(textBox1.Text);
+            int count = 0, k=textBits.Length;
+            int R=0,G=0,B=0;
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    if (textBits.Length == count)
+                    {
+                        break;
+                    }
+                    Color pixel = image.GetPixel(i, j);
+                    R = pixel.R;
+                    G = pixel.G;
+                    B = pixel.B;
+                    if (textBits.Length > count+2)
+                    {
+                        if (R % 2 == 0 && textBits[count] == '1')
+                        {
+                            R = R + 1;
+                        }
+                        if (R % 2 == 1 && textBits[count] == '0')
+                        {
+                            R = R - 1;
+                        }
+                        if (G % 2 == 0 && textBits[count + 1] == '1')
+                        {
+                            G = G + 1;
+                        }
+                        if (G % 2 == 1 && textBits[count + 1] == '0')
+                        {
+                            G = G - 1;
+                        }
+                        if (B % 2 == 0 && textBits[count + 2] == '1')
+                        {
+                            B = B + 1;
+                        }
+                        if (B % 2 == 1 && textBits[count + 2] == '0')
+                        {
+                            B = B - 1;
+                        }
+                    }
+                    else if (textBits.Length == count +2)
+                    {
+                        if (R % 2 == 0 && textBits[count] == '1')
+                        {
+                            R = R + 1;
+                        }
+                        if (R % 2 == 1 && textBits[count] == '0')
+                        {
+                            R = R - 1;
+                        }
+                        if (G % 2 == 0 && textBits[count + 1] == '1')
+                        {
+                            G = G + 1;
+                        }
+                        if (G % 2 == 1 && textBits[count + 1] == '0')
+                        {
+                            G = G - 1;
+                        }
+                    }
+                    else if (textBits.Length == count +1)
+                    {
+                        if (R % 2 == 0 && textBits[count] == '1')
+                        {
+                            R = R + 1;
+                        }
+                        if (R % 2 == 1 && textBits[count] == '0')
+                        {
+                            R = R - 1;
+                        }
+                    }
+                    
+                    Color temp = Color.FromArgb(R,G,B);
+                    image.SetPixel(i, j, temp);
+                    count = count + 3;
+                    if (textBits.Length == count || textBits.Length < count)
+                    {
+                        break;
+                    }
+                    Console.WriteLine(temp);
+                }
+                if (textBits.Length < count)
+                {
+                    break;
+                }      
+            }
+            MessageBox.Show("done!");
+            pictureBox1.Image = image;
+            return image;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                try
+                {
+                    OpenFileDialog open = new OpenFileDialog();
+                    open.Filter = "Image Files(*.bmp)| *.bmp";
+                    if (open.ShowDialog() == DialogResult.OK)
+                    {
+                        textBox2.Text = open.FileName;
+                        bmpPath = open.FileName;
+                        Bitmap bit = new Bitmap(open.FileName);
+                        pictureBox1.Image = bit;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Błąd. Nie można odczytać wskazenego pliku! " + ex.Message + "\n");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "bmp files (*.bmp)|*.bmp|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            try
+            {
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    {
+                        pictureBox1.Image.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                    }
+                    MessageBox.Show("Zapisano!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd. Nie można zapisać wskazenego obrazu! \n " + ex.Message);
+            }
+         
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(bmpPath);
+            imageText = hide(image, textBox1.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            textBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Błąd. Nie można odczytać wskazenego pliku! \n " + ex.Message);
+                }
+            }
+        }
+    }
+}
+
